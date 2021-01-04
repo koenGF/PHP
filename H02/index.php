d4ccd2a90f3afea9e261629d5e9698e77ff328b9
Koen <2144264@talnet.nl>
2021-01-04 16:11:58 +0100

    finished H2, link on homepage

---
+++ H02/index.php
@@ -0,0 +1,28 @@
+<?php
+$a = "Ik heb niet gefaald";
+$b = "die niet werken";
+$c = 10000;
+
+//Ik heb niet gefaald. Ik heb alleen 10000 manieren gevonden die niet werken.
+echo $a."."." Ik heb alleen ".$c." manieren gevonden ".$b."."."<br><br>";
+
+$a = "die geen fout";
+$b = "en";
+$c = "niet.";
+
+//Mensen die geen fouten maken, werken niet.
+echo "Mens".$b." ".$a.$b." mak".$b.", werk".$b." ".$c."<br><br>";
+
+$a = "ry";
+$b = ", but";
+$c = "fail";
+
+//Try and fail, but never fail to try.
+echo "T".$a." and ".$c.$b." never ".$c." to t".$a."."."<br><br>";
+
+$a = "omgaan";
+$b = "met";
+$c = "teleu";
+
+//De cursus “omgaan met teleurstellingen” kan vanavond helaas niet doorgaan.
+echo "De cursus “".$a." ".$b." ".$c."rstellingen"."” kan vanavond helaas niet doorgaan.";

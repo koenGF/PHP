caed62d0171d485db1282910a31531a681039009
Koen <2144264@talnet.nl>
2021-01-07 13:38:52 +0100

    finished H03

--- H03/H03opdr6.php
+++ H03/H03opdr6.php
@@ -1,7 +1,11 @@
 <?php
-$clubs = array("De Spartelaars","De Waterbuffels","Plonsmerin","Bommetje");
-$members = array(25,32,11,23);
-
-foreach ($clubs as $club) {
-    echo $club."<br>";
+$clubs = array ("De Spartelaars" => 25,"De Waterbuffels" => 32,"Plonsmerin" => 11,"Bommetje" => 23);
+$swimImgs = "";
+foreach ($clubs as $key => $value){
+    $numberOfPictures = floor($value / 5);
+    for ($i = 0; $i < $numberOfPictures; $i++) {
+        $swimImgs = $swimImgs."<img src='../img/swim.jpg' alt='swim'>";
+    }
+    echo $key . " " . $value . "$swimImgs" . "<br>";
+    $swimImgs = "";
 }

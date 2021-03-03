113cebea24cd22d0ba7dbcb638b3abbf4ec4bfa9
Koen <2144264@talnet.nl>
2021-03-03 17:15:26 +0100

    added recursiveArrayLoop.php, deleted file.php

---
+++ recursiveArrayLoop.php
@@ -0,0 +1,33 @@
+<?php
+//  Multi-dimensional Source Array
+//  https://stackoverflow.com/questions/3684463/php-foreach-with-nested-array
+
+$newArray = [
+    1, 2, 3,
+    [4, 5],
+    6,
+    [7, [8, 9], 10]
+];
+
+//  Output array
+displayArrayRecursively($newArray);
+
+/**
+ * Recursive function to display members of array with indentation
+ *
+ * @param array $arr Array to process
+ * @param string $indent indentation string
+ */
+function displayArrayRecursively(array $arr, $indent='') {
+    if ($arr) {
+        foreach ($arr as $value) {
+            if (is_array($value)) {
+                //
+                displayArrayRecursively($value, $indent . '-');
+            } else {
+                //  Output
+                echo "$indent $value \n";
+            }
+        }
+    }
+}

9c6e1c6bf7601ad7ff3a571d0c901e2197d101cf
Koen <2144264@talnet.nl>
2022-01-08 16:33:40 +0100

    simplified

--- fibonacci.php
+++ fibonacci.php
@@ -1,19 +1,11 @@
 <?php
 
-$fibonacci[0] = 1;
-$fibonacci[1] = 2;
+$fibonacci = [1,2];
 
 for ($i = 2; $i <= 40; $i++) {
     $fibonacci[$i] = $fibonacci[$i-1] + $fibonacci[$i-2];
 }
 
-displayArray($fibonacci);
-
-
-function displayArray($arr)
-{
-    $arrLength = count($arr);
-    for ($i = 0; $i < $arrLength; $i++) {
-        print($arr[$i] . '<br>');
-    }
+foreach ($fibonacci as $value) {
+    echo $value . "<br>";
 }
